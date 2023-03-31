4765a55778202285b78deea99a260f37d396be23
lee <496090280@qq.com>
2023-03-31 16:26:35 +0800

    feat: 添加事件

--- packages/image/src/index.ts
+++ packages/image/src/index.ts
@@ -1,17 +1,18 @@
 /*
  * @Author: lee
  * @Date: 2022-05-04 20:03:05
- * @LastEditTime: 2022-10-16 16:40:58
+ * @LastEditTime: 2023-03-31 15:36:06
  */
-import App from './index'
-import './index.css'
+import App from "./index.vue";
+import "./index.css";
 
 export default {
   render: App,
   editorProps: {
     src: {
-      type: 'string',
-      defaultValue: 'https://t7.baidu.com/it/u=2621658848,3952322712&fm=193&f=GIF'
-    }
-  }
-}
+      type: "string",
+      defaultValue:
+        "https://t7.baidu.com/it/u=2621658848,3952322712&fm=193&f=GIF",
+    },
+  },
+};
--- packages/image/src/index.vue
+++ packages/image/src/index.vue
@@ -1,11 +1,22 @@
+<!--
+ * @Author: lee
+ * @Date: 2023-03-01 16:32:06
+ * @LastEditTime: 2023-03-27 20:46:41
+-->
 <script setup lang="ts">
-import { defineProps } from 'vue';
+import { defineProps } from "vue";
 
-const { src } = defineProps<{
-  src: string
-}>()
+const { src, events } = defineProps<{
+  src: string;
+  events: Record<string, any[]>;
+}>();
+
+function onClick() {
+  const win = window as any;
+  win.globalEmitter.emit("common:link", events["common:link"]);
+}
 </script>
 
 <template>
-  <img :src="src" class="lc-image" />
+  <img :src="src" class="lc-image" @click="onClick" />
 </template>
